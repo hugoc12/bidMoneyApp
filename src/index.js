bd9dd19aace440b7d907701b0c06d507e881c288
hugoc12 <hugo_c12@outlook.com>
2023-03-13 22:50:59 -0300

    Hugo 13/03 22:50

--- src/index.js
+++ src/index.js
@@ -1,90 +1,77 @@
-import React, {useState, useEffect, useRef} from "react";
-import { View, Text, Modal, TextInput} from "react-native";
-import { ContainerScrollView, TxtSaldo, TxtValor, InputTxt, InputTxt2, Btt, Card, TextInputText, BttReset, ViewModal } from "./styles";
+import React, {useEffect, useState} from "react";
+import { View, Text, Modal } from "react-native";
+import { ContainerScrollView, TxtSaldo, TxtValor, Btt, Card, BttReset, ViewModal } from "./styles";
+import CurrencyInput from "react-native-currency-input";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+let formatNumber = new Intl.NumberFormat('pt-BR', {
+    style:'currency',
+    currency:'BRL',
+})
 
 export default function App(){
-
-    useEffect(()=>{
-        const formatNumber = new Intl.NumberFormat('pt-BR', {
-            style:'currency',
-            currency:'BRL',
-        })
-
-        setVlInput(formatNumber.format('0'));
-        setVlSaldo(formatNumber.format('200'));
-    }, []);
-
-    const [vlInput, setVlInput] = useState(null);
-    const [vlSaldo, setVlSaldo] = useState(null);
-    const [vlInputReset, setVlReset] = useState(null);
+    const [vlInput, setVlInput] = useState(0);
+    const [vlSaldo, setVlSaldo] = useState(formatNumber.format('-250.00'));
+    const [vlInputReset, setVlReset] = useState(0);
     const [modalReset, setModalReset] = useState(false);
-
-    const inputVlRef = useRef(undefined);
-    const inputVlResetRef = useRef(undefined);
-
     const [postedValues, setPostedValues] = useState([]);
 
-    const formatNumber = new Intl.NumberFormat('pt-BR', {
-        style:'currency',
-        currency:'BRL',
-    })
+    useEffect(()=>{
+        async function findData(){
 
-    function editValue(numberTyped, setValue){
+            const saldo = await AsyncStorage.getItem('@saldo');
+            const extratoStr = await AsyncStorage.getItem('@extrato');
+            const extratoArr = extratoStr.split('|').map((el)=>JSON.parse(el));
 
-        if(numberTyped.length >= 3){
-            setValue(formatNumber.format(`${numberTyped.slice(-numberTyped.length, -2)}.${numberTyped.slice(-2)}`));
-        }else if(numberTyped.length == 1){
-            setValue(formatNumber.format(`0.0${numberTyped}`));
-        }else if(numberTyped.length == 2){
-            setValue(formatNumber.format(`0.${numberTyped}`));
-        }else{
-            setValue(formatNumber.format(0));
+            setVlSaldo(formatNumber.format(saldo));
+            setPostedValues(extratoArr);
+            
         }
+
+        findData()
+    }, [])
+
+    async function saveData(saldo, extrato){
+        let valueSaldo = saldo.replace(/\D/g, '').replace(/\s/g, '');
+        valueSaldo = `${valueSaldo.slice(-valueSaldo.length, -2)}.${valueSaldo.slice(-2)}`;
+
+        let arrExtrato = extrato.map((el)=>JSON.stringify(el)); // "{\"key\":data}" -> Retorna o formato de objString que o JSON.parse irá permitir ("key")
+
+        try{
+            await AsyncStorage.setItem('@saldo', valueSaldo);
+            await AsyncStorage.setItem('@extrato', arrExtrato.join('|'));
+        }catch(err){
+            console.log('Erro ao salvar dado!');
+        }
+        
     }
 
     function defValueSaldo(number){
-        setVlSaldo(number);
+        setVlSaldo(formatNumber.format(number));
         setPostedValues([]);
-        setVlReset(formatNumber.format('0'));
-        inputVlResetRef.current.clear();
+        setVlReset(0);
         setModalReset(false);
+        saveData(formatNumber.format(number), []);
     }
 
-    function addValue(number){
+    function editValue(number , type){
         let date = new Date();
-        let value = number ? number.replace(/\D/g, '').replace(/\s/g, '') : false;
-        let value2 = vlSaldo ? vlSaldo.replace(/\D/g, '').replace(/\s/g, '') : false;
-
-        value = `${value.slice(-value.length, -2)}.${value.slice(-2)}`;
-        value2 = `${value2.slice(-value2.length, -2)}.${value2.slice(-2)}`;
-
+        let valueSaldo = vlSaldo.replace(/\D/g, '').replace(/\s/g, '');
+        valueSaldo = `${valueSaldo.slice(-valueSaldo.length, -2)}.${valueSaldo.slice(-2)}`;
         if(vlSaldo[0] == '-'){
-            value2 = Number(value2) * -1;
+            valueSaldo = Number(valueSaldo) * -1; // Negativando o valor antes do calculo, caso o saldo seja negativo.
         }
 
-        setVlSaldo(formatNumber.format(Number(value2) + Number(value)));
-        setPostedValues([{type:true, value:`${value}`, date:`${date.getHours(date)}h${date.getMinutes(date)}m - ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}, ...postedValues])
-        setVlInput(formatNumber.format('0'));
-        inputVlRef.current.clear();
-    }
-
-    function removeValue(number){
-        let date = new Date();
-        let value = number ? number.replace(/\D/g, '').replace(/\s/g, '') : false;
-        let value2 = vlSaldo ? vlSaldo.replace(/\D/g, '').replace(/\s/g, '') : false;
-
-        value = `${value.slice(-value.length, -2)}.${value.slice(-2)}`;
-        value2 = `${value2.slice(-value2.length, -2)}.${value2.slice(-2)}`;
-
-        if(vlSaldo[0] == '-'){
-            value2 = Number(value2) * -1;
+        if(type){
+            setVlSaldo(formatNumber.format(Number(valueSaldo) + Number(number)));
+            saveData(formatNumber.format(Number(valueSaldo) + Number(number)), [{type:type, value:`${formatNumber.format(number)}`, date:`${date.getHours(date)}h${date.getMinutes(date)}m - ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}, ...postedValues]);
+        }else{
+            setVlSaldo(formatNumber.format(Number(valueSaldo) - Number(number)));
+            saveData(formatNumber.format(Number(valueSaldo) - Number(number)), [{type:type, value:`${formatNumber.format(number)}`, date:`${date.getHours(date)}h${date.getMinutes(date)}m - ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}, ...postedValues]);
         }
 
-        setVlSaldo(formatNumber.format(Number(value2) - Number(value)));
-        setPostedValues([{type:false, value:`${value}`, date:`${date.getHours(date)}h${date.getMinutes(date)}m - ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}, ...postedValues])
-        setVlInput(formatNumber.format('0'));
-        inputVlRef.current.clear();
+        setPostedValues([{type:type, value:`${formatNumber.format(number)}`, date:`${date.getHours(date)}h${date.getMinutes(date)}m - ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}, ...postedValues])
+        setVlInput(0);
     }
 
     return(
@@ -92,25 +79,23 @@ export default function App(){
 
             <Modal visible={modalReset} animationType='slide' transparent={true}>
                 <ViewModal>
-                    <Text style={{color:'#EFEFEF', fontSize:30, width:250, borderBottomWidth:1, borderBottomColor:'#fff', textAlign:'center'}}>{vlInputReset}</Text>
-                    <InputTxt2 ref={inputVlResetRef} placeholder="R$" placeholderTextColor='#EFEFEF' keyboardType="numeric" onChangeText={(text)=>editValue(text, setVlReset)}/>
+                    <CurrencyInput value={vlInputReset} onChangeValue={setVlReset} prefix='R$ ' separator="," delimiter="." minValue={0} placeholder='R$ 0,00' placeholderTextColor='#fff'
+                    style={{fontSize:25, color:'#fff',width:300, alignSelf:'center', marginBottom:20 , borderBottomWidth:1, borderBottomColor:'#fff', textAlign:'center'}}/>
                     <Btt colorBtt={'#F66B0E'} style={{marginTop:20}} onPress={(e)=>defValueSaldo(vlInputReset)}>OK</Btt>
-                
                 </ViewModal>
             </Modal>
 
             <TxtSaldo>{vlSaldo}</TxtSaldo>
-
-            <TextInputText>{vlInput}</TextInputText>
-            <InputTxt ref={inputVlRef} keyboardType="numeric" onChangeText={(txt)=>editValue(txt, setVlInput)}/>
+            <CurrencyInput value={vlInput} onChangeValue={setVlInput} prefix='R$ ' separator="," delimiter="." minValue={0} placeholder='R$ 0,00' placeholderTextColor='#fff'
+                style={{fontSize:25, color:'#fff',width:300, alignSelf:'center', marginBottom:20 , borderBottomWidth:1, borderBottomColor:'#fff', textAlign:'center'}}/>
 
             <View style={{flexDirection:"row", alignItems:"center", justifyContent:"space-between"}}>
-                <Btt colorBtt={"#157347"} onPress={()=>addValue(vlInput)}>ADICIONAR</Btt>
-                <Btt colorBtt={"#BB2D3B"} onPress={()=>removeValue(vlInput)}>REMOVER</Btt>
+                <Btt colorBtt={"#157347"} onPress={()=>editValue(vlInput, true)}>ADICIONAR</Btt>
+                <Btt colorBtt={"#BB2D3B"} onPress={()=>editValue(vlInput, false)}>REMOVER</Btt>
             </View>
             <View style={{backgroundColor:"#205375", paddingHorizontal:20, paddingVertical:20}}>
                 {postedValues.map((el, index)=>{
-                    return <Card key={index}><TxtValor colorValor={el.type}>{el.type ? '+':'-'} {el.value}</TxtValor><Text style = {{color:"#EFEFEF", fontSize:18}}>{el.date}</Text></Card>
+                    return <Card key={index}><TxtValor colorValor={el.type}>{el.type ? '+ ':'- '} {el.value}</TxtValor><Text style = {{color:"#EFEFEF", fontSize:18}}>{el.date}</Text></Card>
                 })}
             </View>
 
